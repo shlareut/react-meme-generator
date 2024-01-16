189e9482bac1f507580fc36a0acddbda60409e42
Lars Reuter <lp.reuter@gmail.com>
2024-01-16 22:14:03 +0100

    Add comments

--- src/App.js
+++ src/App.js
@@ -9,3 +9,9 @@ export default function App() {
     </>
   );
 }
+
+// 1. Put HTML structure here
+// 2. Separate out each selector/input as a component
+// 3. Make sure the components are nested correctly
+// e.g. <div className={styles.inputfields}> should come here, with just the component tag <Inputfield /> inside
+// but broken down even more, so each input field is a component!
