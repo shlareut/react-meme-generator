365139acd7f014b2a4c46613bbe95cbfc194845b
Lars Reuter <lp.reuter@gmail.com>
2024-01-16 20:52:05 +0100

    Add input field states

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MemeGenerator.module.scss';
 
 export default function MemeGenerator() {
+  const initialTopText = '';
+  const [topText, setTopText] = useState(initialTopText);
+  const initialBottomText = '';
+  const [bottomText, setBottomText] = useState(initialBottomText);
   return (
     <div className={styles.container}>
       <div className={styles.image}>
@@ -11,11 +15,32 @@ export default function MemeGenerator() {
         <label for="template">Meme template</label>
         <select id="template" name="template" />
         <label for="toptext">Top text</label>
-        <input type="text" id="toptext" name="toptext" />
+        <input
+          type="text"
+          id="toptext"
+          name="toptext"
+          value={topText}
+          onChange={(event) => {
+            const newTopText = event.currentTarget.value;
+            setTopText(newTopText);
+          }}
+        />
         <label for="bottomtext">Bottom text</label>
-        <input type="text" id="bottomtext" name="bottomtext" />
+        <input
+          type="text"
+          id="bottomtext"
+          name="bottomtext"
+          value={bottomText}
+          onChange={(event) => {
+            const newBottomText = event.currentTarget.value;
+            setBottomText(newBottomText);
+          }}
+        />
       </div>
       <div className={styles.button}>Download</div>
+      {/* Below is just for reference and test code */}
+      <p>{topText}</p>
+      <p>{bottomText}</p>
     </div>
   );
 }
